5723e2a1c24a8aeb65cfb61aeb28a32a0c442ace
dutchcelt <studio@dutchcelt.com>
2018-01-27 17:48:59 +0100

    No content, only paths

--- templates/data.js
+++ templates/data.js
@@ -1,10 +1,8 @@
 module.exports = {
-	title: 'Trivet',
 	path: '/',
 	styles: 'styles.css',
 	polyfillService: 'https//cdn.polyfill.io/v2/polyfill.js',
 	polyfillFeatures: 'setImmediate,fetch,Object.create,Promise,es6',
 	systemjs: 'https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.20.19/system.js',
 	trivet: 'trivet.js'
-
 };
