575179f7512c8981c8a6b05b08aa34dc6d447737
dutchcelt <studio@dutchcelt.com>
2020-03-06 22:58:18 +0100

    Simpler solution

--- src/components/layout/main.js
+++ src/components/layout/main.js
@@ -1,22 +1,20 @@
 import layout from 'layout';
 import trivetStyles from 'trivetStyles';
-import { createFragment } from 'insertTemplate';
 
-const template = createFragment(`
-<template>
+const template = document.createElement('template');
+template.innerHTML = `
 	<h1>
 		<slot name="title"></slot>
 	</h1>
 	<slot name="layout" class="layout"></slot>
-</template>
-`);
+`;
 
 customElements.define('trvt-layout',
 	class extends HTMLElement {
 		constructor() {
 			super();
 			const el = this;
-			const shadowRoot = el.attachShadow({mode: 'open'})
+			const shadowRoot = el.attachShadow({mode: 'open'});
 			shadowRoot.adoptedStyleSheets = [trivetStyles,layout];
 			shadowRoot.appendChild(template.content.cloneNode(true));
 		}
