43af50cd934afd7c4e0374a69ad7a528860f7bcb
dutchcelt <studio@dutchcelt.com>
2020-03-29 19:03:45 +0200

    ::slotted bugs :(

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,6 +1,5 @@
 import trvtStyles from 'trvt-styles';
 import shadowStyles from './dropdown.css';
-//import { Templates } from 'Templates';
 import { Atomic } from 'Atomic';
 import createFragment from 'createFragment';
 
@@ -11,7 +10,9 @@ const temp = `
 		<span class="vh expanded-text">expanded</span>
 		<span class="vh collapsed-text">collapsed</span>
 	</label>
-	<slot name="list"></slot>
+	<div role="menu" data-menu-origin="left">
+		<slot name="list"></slot>
+	</div>
 `;
 
 customElements.define('trvt-dropdown',
@@ -19,30 +20,9 @@ customElements.define('trvt-dropdown',
 		constructor() {
 			super();
 			this.attachShadow({ mode: 'open' });
-			this.template = createFragment(temp);
-			this.shadowRoot.appendChild(this.template.cloneNode(true));
 			this.shadowRoot.adoptedStyleSheets = [trvtStyles, shadowStyles];
+			//this.template = createFragment(temp);
+			this.shadowRoot.appendChild(createFragment(temp).cloneNode(true));
 		}
 	}
 );
-
-
-/*
-
-<trvt-dropdown>
-	<input type="checkbox" role="button" aria-haspopup="true" id="toggle" class="vh">
-	<label for="toggle" data-opens-menu>
-		&#x2630; Menu
-	<span class="vh expanded-text">expanded</span>
-	<span class="vh collapsed-text">collapsed</span>
-	</label>
-	<div role="menu" data-menu-origin="left">
-		<ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Contact</a></li>
-		</ul>
-	</div>
-</trvt-dropdown>
-
-*/
