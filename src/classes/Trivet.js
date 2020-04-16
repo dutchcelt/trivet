917bd091a99cc819786a3795f3a309dc3c7c5d28
dutchcelt <studio@dutchcelt.com>
2020-04-16 21:09:41 +0200

    Refactored template wrapping

--- src/classes/Trivet.js
+++ src/classes/Trivet.js
@@ -12,31 +12,24 @@ class Trivet extends HTMLElement {
 			delete this.dataset.cloak;
 		});
 		this.classnames || this.bem();
-		this.template = this.template || this.dynamicTemplate();
-		typeof this.template === 'function' && this.wrapTemplateWithTag();
+		this.template = this.template || this.dynamicDefaultTemplate();
+		if (typeof this.template === 'function') {
+			const target = this.tag ? this.wrapTemplateWithTag() : this.shadowRoot;
+			render(this.template(), target);
+		}
 	}
 
 	wrapTemplateWithTag(){
-
-		const instance = this.template();
-		const target = this.shadowRoot;
-		if(this.tag) {
-			const wrapper = document.createElement(this.tag);
-			wrapper.classList.add(...this.classnames);
-			wrapper.textContent = this.text || null;
-
-			wrapper.innerHTML = wrapper.innerHTML || instance.template.element.innerHTML;
-			instance.template.element.innerHTML = wrapper.outerHTML;
-		}
-		render(instance, target);
+		const wrapper = document.createElement(this.tag);
+		wrapper.classList.add(...this.classnames);
+		return this.shadowRoot.appendChild(wrapper);
 	}
 
-	dynamicTemplate(){
-		return this.querySelector('[slot=default]') ? () => html`<slot name="default"></slot>` : () => html``;
+	dynamicDefaultTemplate(){
+		return this.querySelector('[slot=default]') ? () => html`<slot name="default"></slot>` : () => html`${this.text}`;
 	}
 
 	bem(){
-
 		const getBem = n => this[n] || this.getAttribute(n) || '';
 		const bemObj = {block:'',element:'__',modifier:'--'};
 		const bemArr = Object.keys(bemObj).map(n => getBem(n) && (bemObj[n] + getBem(n)));
