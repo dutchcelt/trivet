712ed2b668655d0132d69f34187d5c88d24e2225
dutchcelt <studio@dutchcelt.com>
2021-11-30 18:14:36 +0000

    removed old code

--- templates/index.js
+++
@@ -1,6 +0,0 @@
-import 'construct-style-sheets-polyfill';
-import normalize from '@csstools/normalize.css' assert { type: 'css' };
-import sheet from 'trvt-styles' assert { type: 'css' };
-import 'init';
-
-document.adoptedStyleSheets = [normalize, sheet];
