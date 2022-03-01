e730d28d8c5a6d9e3c80ba2b7b4c5363c9aee9b4
dutchcelt <studio@dutchcelt.com>
2022-03-01 14:35:15 +0100

    Removed styling from slots

--- webcomponents/layout/src/layout.js
+++ webcomponents/layout/src/layout.js
@@ -1,10 +1,22 @@
 import { styles, contentLoadedMachine } from '@trvt/core';
 import layoutCSS from './layout.css' assert { type: 'css' };
 
+/**
+ *
+ * @param elms
+ * @returns {string[]}
+ * @private
+ */
+const __getSlotNames = (elms) => [...elms]
+		.filter(n => n.hasAttribute('slot'))
+		.map(e => e.getAttribute('slot'));
+
+
 export class TrvtLayout extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+		this.slotNames = __getSlotNames(this.children);
 	}
 
 	connectedCallback() {
@@ -12,20 +24,40 @@ export class TrvtLayout extends HTMLElement {
 		this.shadowRoot.appendChild(this.render());
 		contentLoadedMachine.transition('loading', 'READY')
 	}
+
+	/**
+	 *
+	 * @returns {DocumentFragment}
+	 */
 	render() {
 		const type = this.shadowRoot.host.getAttribute('type');
 		return document.createRange().createContextualFragment(`
 			${!!type ? this.__template() : ``}
 		`);
 	}
+
+	/**
+	 *
+	 * @returns {string}
+	 * @private
+	 */
 	__template() {
-		return `
-			<slot name="navigation"></slot>
-			<slot name="header"></slot>
-			<slot name="main"></slot>
-			<slot name="sidebar"></slot>
-			<slot name="footer"></slot>
-		`;
+		return this.slotNames.map(name => this.__slotMarkupObject()[name] || ``).join(``);
+	}
+
+	/**
+	 *
+	 * @returns {{navigation: string, footer: string, sidebar: string, header: string, main: string}}
+	 * @private
+	 */
+	__slotMarkupObject() {
+		return {
+			"navigation": `<div class="navigation"><slot name="navigation"></slot></div>`,
+			"header":`<div class="header"><slot name="header"></slot></div>`,
+			"main":`<div class="main"><slot name="main"></slot></div>`,
+			"sidebar":`<div class="sidebar"><slot name="sidebar"></slot></div>`,
+			"footer":`<div class="footer"><slot name="footer"></slot></div>`
+		}
 	}
 }
 customElements.define('trvt-layout', TrvtLayout);
