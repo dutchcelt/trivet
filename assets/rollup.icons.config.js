57dab1727ce85ab6c47a594007e31627bee529ce
dutchcelt <studio@dutchcelt.com>
2022-03-05 16:08:31 +0000

    Sass to CSS converisons with a single module

--- assets/rollup.icons.config.js
+++ assets/rollup.icons.config.js
@@ -1,40 +1,40 @@
 const iconFontVersion = `v1.1`;
+const iconFontVariableSource = `./icons/trvt-icons-${iconFontVersion}/variables.scss`;
 
 /**
  * convert icon font to woff2 format
  */
 import wawoff from 'wawoff2';
 import fs from 'fs';
-const sourceFile = fs.readFileSync(`./icons/trvt-icons-${iconFontVersion}/fonts/trvt-icons.ttf`);
+const sourceFile = fs.readFileSync(
+	`./icons/trvt-icons-${iconFontVersion}/fonts/trvt-icons.ttf`
+);
 const iconFile = './build/trvt-icons.woff2';
 
-wawoff.compress(sourceFile).then(convertedFile => {
-	fs.writeFileSync(iconFile, convertedFile)
-  });
-
+wawoff.compress(sourceFile).then((convertedFile) => {
+	fs.writeFileSync(iconFile, convertedFile);
+});
 
 /**
  * Convert a scss var file to a css :root custom property file
  */
-import { sassToCssVars } from './scripts/sassToCssVars.js';
-const iconglyphsObject = {
-	src: `./icons/trvt-icons-${iconFontVersion}/variables.scss`, 
-	dest: './build/glyphs.css'
-}
-sassToCssVars.convert(iconglyphsObject);
-
-
+import { sassToCss } from './scripts/sassToCss.js';
+sassToCss.convert({
+	src: iconFontVariableSource,
+	dest: './build/glyphs.css',
+	rule: `:root `,
+	withPropValues: true,
+});
 
 /**
- * Convert a scss var file to a css file with classes to 
+ * Convert a scss var file to a css file with classes to
  */
- import { createIconClasses } from './scripts/createIconClasses.js';
- const iconClassesObject = {
-	 src: `./icons/trvt-icons-${iconFontVersion}/variables.scss`, 
-	 dest: './build/trvt-icon-classes.css'
- }
- createIconClasses.convert(iconClassesObject);
- 
+sassToCss.convert({
+	src: iconFontVariableSource,
+	dest: './build/trvt-icon-classes.css',
+	rule: `@layer designsystem `,
+	withPropValues: false,
+});
 
 import styles from 'rollup-plugin-styles';
 export default {
