c8ec522bac0765ff52cf38ef8a262b2207ff7c4b
egor <studio@dutchcelt.com>
2022-03-08 10:44:32 +0100

    Preventing unwanted detail values

--- webcomponents/core/src/dataBus.js
+++ webcomponents/core/src/dataBus.js
@@ -1,13 +1,36 @@
+const sanitizeThis = (str) => {
+	const el = document.createElement('div');
+	el.innerText = str;
+	return el.innerHTML;
+}
+
 class EventDataBus {
 	constructor() {
 		this._bus = document.createElement('div');
 		this.data =  {};
 	}
 	_storeDetail(name, detail = {}){
+		detail = this._safeValues(detail);
 		this.data[name]
 			? Object.assign(this.data[name].detail, detail)
 			: this.data[name] = { detail };
 		return this.data[name];
+	}
+	_retrieveDetail(name) {
+		return this.data[name] && this.data[name].detail;
+	}
+	_safeValues(detail){
+		const cleanDetail = {};
+		for (const [key, value] of Object.entries(detail)) {
+			const propType = typeof detail[key];
+			const safeType = (/string|boolean|number/).test(propType)
+			if (safeType) {
+				cleanDetail[key] = safeType === 'string' ? sanitizeThis(value) : value;
+			} else {
+				console.warn(`Trivet: Detail property '${key}' of type '${propType}' is prohibited and has been removed`);
+			}
+		}
+		return cleanDetail;
 	}
 	register(event, callback) {
 		this._bus.addEventListener(event, callback);
@@ -24,7 +47,7 @@ class EventDataBus {
 		this._storeDetail(name, detail);
 	}
 	getDetail(name, property) {
-		const detail = this._storeDetail(name).detail;
+		const detail = this._retrieveDetail(name);
 		return detail && property ? detail[property] : detail;
 	}
 }
