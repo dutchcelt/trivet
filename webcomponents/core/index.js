d563aeff41d117517d67cabd8009b5fc1d24e9db
dutchcelt <studio@dutchcelt.com>
2022-11-12 14:26:47 +0000

    feature: export ShadowStyles class

--- webcomponents/core/index.js
+++ webcomponents/core/index.js
@@ -3,3 +3,4 @@ export { dataBus } from './src/dataBus.js';
 export { fontsLoader } from './src/fontsLoader.js';
 export { defineElement } from './src/defineElement.js';
 export { createFragment } from './src/createFragment.js';
+export { ShadowStyles } from './src/shadowStyles.js';
