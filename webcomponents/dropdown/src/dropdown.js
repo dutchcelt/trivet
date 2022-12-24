eb4e82d19fec97548420973f93435ef39309e107
dutchcelt <studio@dutchcelt.com>
2022-12-24 15:37:15 +0000

    fix: avoiding host context styling

--- webcomponents/dropdown/src/dropdown.js
+++ webcomponents/dropdown/src/dropdown.js
@@ -16,19 +16,34 @@ export class TrvtDropdown extends HTMLElement {
 	menuTemplateFunction() {
 		return html`
 			<nav class="${this.bem()}">
-				<input type="checkbox" role="button" aria-haspopup="true" id="toggle" class="hidden" />
+				<input
+					type="checkbox"
+					role="button"
+					aria-haspopup="true"
+					id="toggle"
+					class="hidden"
+				/>
 				<label for="toggle" data-opens-menu>
 					&#x2630; Menu
 					<span class="hidden expanded-text">expanded</span>
 					<span class="hidden collapsed-text">collapsed</span>
 				</label>
 
-				<div role="menu" class="${this.bem('menu')}" data-menu-origin="left">
+				<div
+					role="menu"
+					class="${this.bem('menu')}"
+					data-menu-origin="left"
+				>
 					<ul class="${this.bem('list')}">
 						${this.menu.map(
 							(item) => html`
 								<li class="${this.bem('item')}">
-									<a class="${this.bem('link')}" href="${item.url}"> ${item.text} </a>
+									<a
+										class="${this.bem('link')}"
+										href="${item.url}"
+									>
+										${item.text}
+									</a>
 								</li>
 							`
 						)}
