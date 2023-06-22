dca34746da980d4c59c685ac8f37c38550ec8447
dutchcelt <studio@dutchcelt.com>
2023-06-22 15:53:15 +0200

    Functionall button

--- webcomponents/button/src/button.js
+++ webcomponents/button/src/button.js
@@ -3,37 +3,44 @@ import buttonCSS from './button.css' assert { type: 'css' };
 
 export class TrvtButton extends HTMLElement {
 	static formAssociated = true;
-
+	static observedAttributes = ['data-trvt-value'];
+	#internals;
+	#shadowRoot;
+	#type;
+	#value;
 	constructor() {
 		super();
-		this.internals = this.attachInternals();
+		this.#internals = this.attachInternals();
+		this.#shadowRoot = this.attachShadow({ mode: 'closed' });
+		this.#shadowRoot.adoptedStyleSheets = [...styles, buttonCSS];
 
-		this.attachShadow({ mode: 'open' });
-		this.shadowRoot.adoptedStyleSheets = [...styles, buttonCSS];
-		this.id = '';
-		this.name = '';
-		this.hidden = false;
-		this.disabled = false;
-		this.name = '';
-		this.trvtText = this.dataset.trvtText || '';
-		this.trvtText = this.dataset.trvtText || '';
-		this.trvtValue = this.dataset.trvtValue || '';
-		this.trvtType = this.dataset.trvtType || 'button';
+		this.#type = this.dataset.trvtType || 'button';
+		delete this.dataset.trvtType;
+		this.#value = this.dataset.trvtValue || '';
 
-		this.shadowRoot.appendChild(this.render());
+		this.trvtDisabled = this.dataset.trvtDisabled || false;
+		this.#shadowRoot.appendChild(this.#render());
+		this.button = this.#shadowRoot.querySelector('button');
+		this.button.addEventListener('click', () => {
+			this.#internals.form && this.#internals.setFormValue(this.#value);
+			if (this.#type === 'submit') this.#internals.form.submit();
+		});
 	}
-	render() {
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.#value = newValue;
+		this.#internals.form && this.#internals.setFormValue(newValue);
+	}
+	#render() {
 		return document.createRange().createContextualFragment(`
 			<button 
-				type="${this.trvtType}"
-				value="${this.trvtValue}"
-				disabled="${this.trvtType}"
-				hidden="${this.hidden}"
-				disabled="${this.disabled}"
-				id="${this.id}"
-				name="${this.name}"
-				>
-				${this.trvtText}
+				type="${this.#type}"
+				${!!this.hidden && ` hidden="true"`}
+				${!!this.#value && ` value="${this.#value}"`}
+				${!!this.trvtDisabled && ` disabled="true"`}
+				${!!this.id && ` id="${this.id}"`}
+				${!!this.name && ` name="${this.name}"`}
+			>
+				<slot></slot>
 			</button>
 		`);
 	}
