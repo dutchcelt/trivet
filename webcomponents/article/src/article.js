56cc7731d1722f104f46d15844a4141a4af2ab6c
dutchcelt <studio@dutchcelt.com>
2023-06-23 15:22:53 +0200

    Card & Article now share the same content class for custom elements

--- webcomponents/article/src/article.js
+++ webcomponents/article/src/article.js
@@ -1,52 +1,10 @@
-import { styles, createFragment } from '@trvt/core';
+import { ContentElementClass } from '@trvt/core';
 import articleCSS from './article.css' assert { type: 'css' };
-const articleTypes = Object.freeze({
-	page: 'H1',
-	default: 'H2',
-	summary: 'H3',
-});
 
-export class TrvtArticle extends HTMLElement {
-	#tag;
+export class TrvtArticle extends ContentElementClass {
 	constructor() {
 		super();
-		this.attachShadow({ mode: 'open' });
-		this.shadowRoot.adoptedStyleSheets = [...styles, articleCSS];
-
-		this.type = this.getAttribute('type');
-		this.#tag = articleTypes[this.type] || articleTypes.default;
-		this.shadowRoot.appendChild(this.render());
-	}
-
-	getHeadingTagName() {
-		const headingTagName = this.querySelector("[slot='heading']")?.tagName;
-		const isValidHeadingTag = Object.values(articleTypes).includes(
-			headingTagName || ''
-		);
-		return (
-			(isValidHeadingTag && '') || articleTypes[this.type || 'default']
-		);
-	}
-
-	headingTemplate() {
-		const slot = `<slot name="heading"></slot>`;
-		let headingElement;
-		if (this.type) {
-			headingElement = document.createElement(this.#tag);
-			headingElement.innerHTML = slot;
-		}
-		return this.type ? headingElement.outerHTML : slot;
-	}
-
-	render() {
-		return createFragment(`
-			<article>
-				${this.headingTemplate()}
-				<slot name="intro"></slot>
-        <slot name="content"></slot>
-        <slot name="footer"></slot>
-			</article>
-		`);
+		this.shadowRoot.adoptedStyleSheets = [articleCSS];
 	}
 }
 customElements.define('trvt-article', TrvtArticle);
--- webcomponents/card/src/card.js
+++ webcomponents/card/src/card.js
@@ -1,21 +1,10 @@
-import { styles, createFragment } from '@trvt/core';
+import { ContentElementClass } from '@trvt/core';
 import cardCSS from './card.css' assert { type: 'css' };
 
-export class TrvtCard extends HTMLElement {
+export class TrvtCard extends ContentElementClass {
 	constructor() {
 		super();
-		this.attachShadow({ mode: 'open' });
-		this.shadowRoot.adoptedStyleSheets = [...styles, cardCSS];
-		this.compTitle = this.dataset?.compTitle || ``;
-		this.shadowRoot.appendChild(this.render());
-	}
-	render() {
-		return createFragment(`
-			<article>	
-				${this.compTitle && `<h2>${this.compTitle}</h2>`}
-				<slot></slot>
-			</article>
-		`);
+		this.shadowRoot.adoptedStyleSheets = [cardCSS];
 	}
 }
 customElements.define('trvt-card', TrvtCard);
