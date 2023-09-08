145032d87ab05d6c33179bf70a37e3b6f8e86e76
Egor Kloos <egor.kloos@ns.nl>
2023-09-08 14:19:24 +0200

    Comment with new improved solution

--- cli/lib/defaults.js
+++ cli/lib/defaults.js
@@ -4,6 +4,10 @@ const trivetTokensPath = getInstalledPathSync('@trvt/designtokens', {
 	paths: process.mainModule.paths,
 });
 
+// const shell = require('shelljs');
+// const defaultTokenPath = shell.exec(`npm ls @trvt/designtokens -ps`, { silent: true }).stdout;
+// const tokenPath = path.join(defaultTokenPath.trim(), `tokens`);
+
 module.exports = {
 	buildPath: path.resolve(`build`, `css`),
 	layer: 'design.tokens',
