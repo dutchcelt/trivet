b6e9afcbc62447ca01db8910a20e95d80facb26b
dutchcelt <studio@dutchcelt.com>
2023-11-05 17:43:18 +0100

    Build of all packages and a minor css fix for FOUC

--- webcomponents/article/build/index.js
+++ webcomponents/article/build/index.js
@@ -1,12 +1,1 @@
-import { TrivetElement as t } from '@trvt/core';
-import s from './styles-99a46e24.css' assert { type: 'css' };
-class e extends t {
-	constructor() {
-		super(),
-			(this.shadowStyleSheets = s),
-			(this.template =
-				'\n\t<article>\n\t\t<slot name="heading"></slot>\n\t\t<slot name="intro"></slot>\n\t\t<slot name="content"></slot>\n\t\t<slot name="footer"></slot>\n\t\t<slot></slot>\n\t</article>\n');
-	}
-}
-customElements.define('trvt-article', e);
-export { e as TrvtArticle };
+import{TrivetElement as t}from"@trvt/core";import s from"./styles-99a46e24.css"assert{type:"css"};class e extends t{constructor(){super(),this.shadowStyleSheets=s,this.template='\n\t<article>\n\t\t<slot name="heading"></slot>\n\t\t<slot name="intro"></slot>\n\t\t<slot name="content"></slot>\n\t\t<slot name="footer"></slot>\n\t\t<slot></slot>\n\t</article>\n'}}customElements.define("trvt-article",e);export{e as TrvtArticle};
