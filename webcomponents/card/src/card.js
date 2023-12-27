e4005a52b7b07d6ed95337b21850fd4bfb655d73
dutchcelt <studio@dutchcelt.com>
2023-12-27 14:41:18 +0100

    Refactored card

--- webcomponents/card/src/card.js
+++ webcomponents/card/src/card.js
@@ -1,21 +1,47 @@
-import { TrivetElement } from '@trvt/core';
-import cardCSS from './card.css' assert { type: 'css' };
+import {TrivetElement} from '@trvt/core';
+import cardCSS from './card.css' assert {type: 'css'};
 
-const contentTemplate = `
-	<article>
-		<slot name="heading"></slot>
-		<slot name="intro"></slot>
-		<slot name="content"></slot>
-		<slot name="footer"></slot>
-		<slot></slot>
-	</article>
-`;
+/**
+ * @constant
+ * @type {string}
+ * @description Holds the custom name for the custom HTML element
+ **/
+const ELEMENT_NAME = 'trvt-card';
 
+/**
+ * @class
+ * @classdesc Represents a TrvtCard which extends TrivetElement.
+ * @extends TrivetElement
+ */
 export class TrvtCard extends TrivetElement {
+	/**
+	 * @constructor
+	 * @description Instantiates TrvtCard class and sets the shadow style sheet and the template.
+	 */
 	constructor() {
 		super();
+		/** @type {CSSStyleSheet} */
 		this.shadowStyleSheets = [cardCSS];
-		this.template = contentTemplate;
+		this.template = this.contentTemplate();
+	}
+
+	/**
+	 * Generate the content in HTML format
+	 * @method
+	 * @returns {string} The HTML content template.
+	 */
+	contentTemplate() {
+		return `
+			<article>
+				<slot name="heading"></slot>
+				<slot name="intro"></slot>
+				<slot name="content"></slot>
+				<slot name="footer"></slot>
+				<slot></slot>
+			</article>
+		`;
 	}
 }
-customElements.define('trvt-card', TrvtCard);
+
+customElements.define(ELEMENT_NAME, TrvtCard);
+customElements.define(ELEMENT_NAME, TrvtCard);
