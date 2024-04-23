87dee4b8424f027be8e055e6e083533d034d0cb0
dutchcelt <studio@dutchcelt.com>
2024-04-23 15:50:11 +0200

    Removed emit Declaration only

--- rollup-preserve-css-modules/index.js
+++ rollup-preserve-css-modules/index.js
@@ -4,11 +4,11 @@ import { createRequire } from 'module';
 import { asyncWalk } from 'estree-walker';
 import MagicString from 'magic-string';
 
-import { 
-  isStaticCssImport, 
-  isDynamicCssImport, 
-  isTemplateStringWithVariables, 
-  isBinaryExpression 
+import {
+	isStaticCssImport,
+	isDynamicCssImport,
+	isTemplateStringWithVariables,
+	isBinaryExpression
 } from './src/ast.js';
 import { getSourceHash, isBareModuleSpecifier } from './src/utils.js';
 
@@ -23,118 +23,118 @@ const ignoredProtocols = ['data:', 'http:', 'https:'];
  * @return {import('rollup').Plugin}
  */
 export default function css(options = {}) {
-  const cssFilesMap = {};
+	const cssFilesMap = {};
 
-  return {
-    name: 'css',
-    resolveId(id) {
-      if (id.endsWith('.css')) {
-        return {
-          id,
-          external: true
-        }
-      }
-    },
-    async transform(code, id) {
-      if (!id.endsWith('.css')) {
-        const ast = this.parse(code);
-        const magicString = new MagicString(code);
-        let modifiedCode = false;
+	return {
+		name: 'css',
+		resolveId(id) {
+			if (id.endsWith('.css')) {
+				return {
+					id,
+					external: true
+				};
+			}
+		},
+		async transform(code, id) {
+			if (!id.endsWith('.css')) {
+				const ast = this.parse(code);
+				const magicString = new MagicString(code);
+				let modifiedCode = false;
 
-        await asyncWalk(ast, {
-          enter: async node => {
-            if (
-              isStaticCssImport(node) || 
-              isDynamicCssImport(node)
-            ) {
-              if(
-                /**
-                 * @example `import(`./foo-${i}.css`, { with: { type: 'css'} })`
-                 */
-                isTemplateStringWithVariables(node) ||
-                /**
-                 * @example `import('./foo-' + i + '.css', { with: { type: 'css'} })`
-                 */
-                isBinaryExpression(node)
-              ) {
-                console.warn(`
+				await asyncWalk(ast, {
+					enter: async node => {
+						if (
+							isStaticCssImport(node) ||
+							isDynamicCssImport(node)
+						) {
+							if (
+								/**
+								 * @example `import(`./foo-${i}.css`, { with: { type: 'css'} })`
+								 */
+								isTemplateStringWithVariables(node) ||
+								/**
+								 * @example `import('./foo-' + i + '.css', { with: { type: 'css'} })`
+								 */
+								isBinaryExpression(node)
+							) {
+								console.warn(`
 [ROLLUP-PLUGIN-NATIVE-CSS-MODULES]: Dynamic imports with variables are not supported, since they rely on runtime code they are hard to statically analyze.
 
 Found in module "${id}": 
 
 ${code.substring(node.start, node.end)}
 `);
-                return;
-              }
+								return;
+							}
 
-              if(
-                node.source.type !== 'Literal' &&
-                node.source.type !== 'TemplateLiteral'
-              ) {
-                return;
-              }
+							if (
+								node.source.type !== 'Literal' &&
+								node.source.type !== 'TemplateLiteral'
+							) {
+								return;
+							}
 
-              const moduleSpecifier = /** @type {string} */ (node.source.value || node.source.quasis[0].value.raw);
+							const moduleSpecifier = /** @type {string} */ (node.source.value || node.source.quasis[0].value.raw);
 
-              /** Ignore external css files or data URIs */
-              if(ignoredProtocols.some(protocol => moduleSpecifier.startsWith(protocol))) {
-                return;
-              }
+							/** Ignore external css files or data URIs */
+							if (ignoredProtocols.some(protocol => moduleSpecifier.startsWith(protocol))) {
+								return;
+							}
 
-              const dirname = path.dirname(id);
-              const absolutePathToCssModule = isBareModuleSpecifier(moduleSpecifier)
-                ? require.resolve(moduleSpecifier)
-                : path.join(dirname, moduleSpecifier);
+							const dirname = path.dirname(id);
+							const absolutePathToCssModule = isBareModuleSpecifier(moduleSpecifier)
+								? require.resolve(moduleSpecifier)
+								: path.join(dirname, moduleSpecifier);
 
-              /** If we havent processed this file before */
-              if (!cssFilesMap[absolutePathToCssModule]) {
-                const cssModuleContentsBuffer = await fs.readFile(absolutePathToCssModule);
-                const cssModuleContents = await cssModuleContentsBuffer.toString();
+							/** If we havent processed this file before */
+							if (!cssFilesMap[absolutePathToCssModule]) {
+								const cssModuleContentsBuffer = await fs.readFile(absolutePathToCssModule);
+								const cssModuleContents = await cssModuleContentsBuffer.toString();
 
-                const assetSource = options?.transform
-                  ? await options?.transform(cssModuleContents)
-                  : cssModuleContents;
+								const assetSource = options?.transform
+									? await options?.transform(cssModuleContents)
+									: cssModuleContents;
 
-                const hash = getSourceHash(assetSource);
-                const assetName = `styles-${hash}.css`;
-                cssFilesMap[absolutePathToCssModule] = assetName;
+								const hash = getSourceHash(assetSource);
+								const assetName = `styles-${hash}.css`;
+								cssFilesMap[absolutePathToCssModule] = assetName;
 
-                this.emitFile({
-                  type: 'asset',
-                  fileName: assetName,
-                  source: assetSource
-                });
+								this.emitFile({
+									type: 'asset',
+									fileName: assetName,
+									source: assetSource
+								});
 
-                magicString.overwrite(
-                  node.source.start,
-                  node.source.end,
-                  `'./${assetName}'`
-                );
+								magicString.overwrite(
+									node.source.start,
+									node.source.end,
+									`'./${assetName}'`
+								);
 
-                modifiedCode = true;
-              } else {
-                magicString.overwrite(
-                  node.source.start,
-                  node.source.end,
-                  `'./${cssFilesMap[absolutePathToCssModule]}'`
-                );
+								modifiedCode = true;
+							} else {
+								magicString.overwrite(
+									node.source.start,
+									node.source.end,
+									`'./${cssFilesMap[absolutePathToCssModule]}'`
+								);
 
-                modifiedCode = true;
-              }
-            }
-          }
-        });
+								modifiedCode = true;
+							}
+						}
+					}
+				});
 
-        if (modifiedCode) {
-          return {
-            code: magicString.toString(),
-            map: magicString.generateMap({ hires: true }),
-          };
-        }
+				if (modifiedCode) {
+					return {
+						code: magicString.toString(),
+						map: magicString.generateMap({ hires: true })
+					};
+				}
 
-        return null;
-      }
-    }
-  }
+				return null;
+			}
+		}
+	};
 }
 
