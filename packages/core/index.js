5c79d9b8dbdff9b4ef7441b57ac2adaeffb2386e
dutchcelt <studio@dutchcelt.com>
2024-08-18 13:19:20 +0200

    Removed eventbus. Not being used.

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,4 @@
 export { styles, TrivetElement, cssLayerDefinitions } from './src/core.js';
-export { dataBus } from './src/dataBus.js';
 export { fontsLoader } from './src/fontsLoader.js';
 export { createFragment } from './src/createFragment.js';
 export { FormMixin } from './src/formMixin.js';
--- packages/core/tests/core.test.js
+++ packages/core/tests/core.test.js
@@ -1,5 +1,5 @@
 import { expect, assert } from '@esm-bundle/chai';
-import { styles, dataBus } from '../index.js';
+import { styles } from '../index.js';
 
 describe('Core Styles ', async () => {
 	it('Style is array', () => {
@@ -10,30 +10,5 @@ describe('Core Styles ', async () => {
 		const ss = styles[0].cssRules[0];
 		expect(ss.constructor.name).to.equal('CSSLayerStatementRule');
 		//expect(/@layer normalize/gi.test(ss.cssText)).to.be.true;
-	});
-});
-
-const testEvent = event => {
-	event.detail.callbackTest = event.detail.testVal;
-};
-dataBus.register('testEvent', testEvent);
-
-describe('Databus tests ', async () => {
-	it('Eventbus callback detail', () => {
-		const testVal = 'tested';
-		dataBus.fire('testEvent', { testVal });
-		const detail = dataBus['testEvent'].data;
-		expect(detail.callbackTest).to.equal(testVal);
-	});
-	it('Eventbus add detail', () => {
-		dataBus.fire('testEvent', { added: 'addedDetail' });
-		expect(dataBus['testEvent'].data.added).to.equal('addedDetail');
-	});
-	it('Eventbus get all detail keys', () => {
-		const keys = Object.keys(dataBus['testEvent'].data);
-		expect(keys.length).to.equal(3);
-		expect(keys).to.include('testVal');
-		expect(keys).to.include('callbackTest');
-		expect(keys).to.include('added');
 	});
 });
