b51ecc2351b190d695872dd5489426c75fdaccbd
dutchcelt <studio@dutchcelt.com>
2024-08-24 13:06:26 +0200

    Reduced layer complexity and reduced repeated base selector properties

--- packages/core/tests/core.test.js
+++ packages/core/tests/core.test.js
@@ -4,7 +4,7 @@ import { styles } from '../index.js';
 describe('Core Styles ', async () => {
 	it('Style is array', () => {
 		assert.isArray(styles);
-		expect(styles.length).to.equal(6);
+		expect(styles.length).to.equal(4);
 	});
 	it('First rule is CSSLayerStatementRule', () => {
 		const ss = styles[0].cssRules[0];
