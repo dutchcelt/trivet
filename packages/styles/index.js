f3d72cf3543fb08df4b95dd2494ab058cd680ea3
dutchcelt <studio@dutchcelt.com>
2024-08-24 11:48:53 +0200

    Some type fixes

--- packages/styles/index.js
+++ packages/styles/index.js
@@ -1,11 +1,15 @@
-// @ts-nocheck
-
 /* Styles */
+// @ts-expect-error
 import layersCSS from './src/index.css' with { type: 'css' };
+// @ts-expect-error
 import resetCSS from './src/base/reset.css' with { type: 'css' };
+// @ts-expect-error
 import defaultsCSS from './src/base/defaults.css' with { type: 'css' };
+// @ts-expect-error
 import tokensCSS from './src/design/tokens.css' with { type: 'css' };
+// @ts-expect-error
 import foundationCSS from './src/design/foundation.css' with { type: 'css' };
+// @ts-expect-error
 import utilitiesCSS from './src/design/utilities.css' with { type: 'css' };
 
 /**
